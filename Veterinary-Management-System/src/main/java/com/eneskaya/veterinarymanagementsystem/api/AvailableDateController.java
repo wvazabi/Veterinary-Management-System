273b52aedbd10ac24ad1438f76599a12427f01e8
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-22 14:39:30 +0300

    Available Date Controller

--- Veterinary-Management-System/src/main/java/com/eneskaya/veterinarymanagementsystem/api/AvailableDateController.java
+++ Veterinary-Management-System/src/main/java/com/eneskaya/veterinarymanagementsystem/api/AvailableDateController.java
@@ -3,6 +3,7 @@ package com.eneskaya.veterinarymanagementsystem.api;
 import com.eneskaya.veterinarymanagementsystem.business.abstracts.IAvailableDateService;
 import com.eneskaya.veterinarymanagementsystem.business.abstracts.IDoctorService;
 import com.eneskaya.veterinarymanagementsystem.core.config.modelMapper.IModelMapperService;
+import com.eneskaya.veterinarymanagementsystem.core.result.Result;
 import com.eneskaya.veterinarymanagementsystem.core.result.ResultData;
 import com.eneskaya.veterinarymanagementsystem.core.utilies.ResultHelper;
 import com.eneskaya.veterinarymanagementsystem.dto.request.animal.AnimalUpdateRequest;
@@ -10,9 +11,11 @@ import com.eneskaya.veterinarymanagementsystem.dto.request.availableDate.Availab
 import com.eneskaya.veterinarymanagementsystem.dto.request.availableDate.AvailableDateUpdateRequest;
 import com.eneskaya.veterinarymanagementsystem.dto.response.animal.AnimalResponse;
 import com.eneskaya.veterinarymanagementsystem.dto.response.availableDate.AvailableDateResponse;
+import com.eneskaya.veterinarymanagementsystem.dto.response.cursor.CursorResponse;
 import com.eneskaya.veterinarymanagementsystem.entities.Animal;
 import com.eneskaya.veterinarymanagementsystem.entities.AvailableDate;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,4 +55,32 @@ public class AvailableDateController {
         availableDateResponse.setAvailableDate(updateAvailableDate.getAvailableDate());
         return ResultHelper.createData(availableDateResponse);
     }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<AvailableDateResponse> get(@PathVariable("id") Long id) {
+        AvailableDate availableDate = this.availableDateService.get(id);
+        AvailableDateResponse availableDateResponse = this.modelMapper.forResponse().map(availableDate, AvailableDateResponse.class);
+        return ResultHelper.successData(availableDateResponse);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<AvailableDateResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize
+    ) {
+        Page<AvailableDate> availableDatesPage = this.availableDateService.cursor(page, pageSize);
+        Page<AvailableDateResponse> availableDatesResponsePage = availableDatesPage.map(availableDate -> this.modelMapper.forResponse().map(availableDate, AvailableDateResponse.class));
+        return ResultHelper.cursor(availableDatesResponsePage);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") Long id) {
+        this.availableDateService.delete(id);
+        return ResultHelper.ok();
+    }
+
+
 }
